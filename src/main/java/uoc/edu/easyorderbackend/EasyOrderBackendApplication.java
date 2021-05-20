9036b6ae91f47791e31115f262d247122a51ad51
visape <vikktor.1996@hotmail.com>
2021-05-21 01:07:27 +0200

    Stripe api key added

--- src/main/java/uoc/edu/easyorderbackend/EasyOrderBackendApplication.java
+++ src/main/java/uoc/edu/easyorderbackend/EasyOrderBackendApplication.java
@@ -1,23 +1,15 @@
 package uoc.edu.easyorderbackend;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.stripe.Stripe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.DirectoryStream;
+import uoc.edu.easyorderbackend.constants.EasyOrderConstants;
 
 @SpringBootApplication
 public class EasyOrderBackendApplication {
 
 	public static void main(String[] args) {
+		Stripe.apiKey = EasyOrderConstants.STRIPE_API_KEY;
 		SpringApplication.run(EasyOrderBackendApplication.class, args);
 	}
 }
--- src/main/java/uoc/edu/easyorderbackend/constants/EasyOrderConstants.java
+++ src/main/java/uoc/edu/easyorderbackend/constants/EasyOrderConstants.java
@@ -7,4 +7,6 @@ public final class EasyOrderConstants {
     public static final String notPaidOrderState = "NOT_PAID";
     public static final String paidOrderState = "PAID";
     public static final String canceledOrderState = "CANCELED";
+
+    public static final String STRIPE_API_KEY = "sk_test_51ItFJUK1ZW7F7TQdI01KhBvmotihncFKdO89oK4oZ5Qzuq6AbPELUVEIEl1CxzToeTSE6eN9sfxhW4g6ce951UZN0068NMcGlw";
 }
